2c727c391c045dfed1868ff7ec7bdfcd06d61184
vshy108 <808926+vshy108@users.noreply.github.com>
2025-06-17 14:40:45 +0800

    💄 Display all cards in one screen

--- src/components/TarotBoard.vue
+++ src/components/TarotBoard.vue
@@ -5,13 +5,17 @@ const cards = Array.from({ length: 78 }, (_, i) => i + 1)
 </script>
 
 <template>
-  <div class="grid grid-cols-6 gap-4 p-4">
+  <div class="w-full h-full p-2">
     <div
-      v-for="card in cards"
-      :key="card"
-      class="w-24 h-36"
+      class="grid grid-cols-[repeat(auto-fit,minmax(3rem,1fr))] gap-1"
     >
-      <CardBack />
+      <div
+        v-for="card in cards"
+        :key="card"
+        class="w-full aspect-[2/3] max-w-[4rem] mx-auto"
+      >
+        <CardBack />
+      </div>
     </div>
   </div>
 </template>
