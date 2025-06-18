e9e5acade7108ff4b5c8b9d2900d09c98d9ebec4
vshy108 <808926+vshy108@users.noreply.github.com>
2025-06-18 10:04:02 +0800

    🚧 Install gsap and add basic draw taraot card view
    Add Tarot card modal

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TarotCardBoard from "@/components/TarotCardBoard.vue";
+// import TarotCardBoard from "@/components/TarotCardBoard.vue";
+import DrawTarotCardView from "@/components/DrawTarotCardView.vue";
 </script>
 
 <template>
   <main class="min-h-screen text-black flex items-center justify-center">
-    <TarotCardBoard />
+    <DrawTarotCardView />
   </main>
 </template>
